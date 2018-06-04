TriggerMail/rules_pyz

commit 3c704e909aa6992466f02f8b52beb14400ce2e3b

tests/python_envvars.py: Check that PYTHON_* env vars are passed (#11)
We want to strip some PYTHON* environment variables that can change import
paths, since these can "break" pyz_binaries. For example, if executing a
pyz_binary while inside a virtual environment. However, this broke a
program that used an unrelated environment variable named PYTHON_SOMETHING.
Be conservative about what environment variables to disable.

tests/BUILD
@@ -315,3 +315,14 @@ pyz_output_test(
     pyz_binary=":run_other",
     expected_output="run_other output: hello world",
 )
+
+pyz_binary(
+    name="python_envvars",
+    srcs=["python_envvars.py"],
+)
+pyz_output_test(
+    name="python_envvars_test",
+    pyz_binary="python_envvars",
+    expected_output="PYTHON_EXAMPLE=should be found",
+    extra_arg="$(location :python_envvars_exezip)",
+)

rules_python_zip/linkzip.py
@@ -20,8 +20,7 @@ def copy_files(zf, files_json):
         # https://stackoverflow.com/questions/39296101/python-zipfile-removes-execute-permissions-from-binaries
         src_stat = os.stat(src)
         zinfo = zipfile.ZipInfo(dst, time.gmtime(src_stat.st_mtime))
-        if src_stat.st_mode & stat.S_IXUSR:
-            zinfo.external_attr = src_stat.st_mode << 16
+        zinfo.external_attr = src_stat.st_mode << 16
         zf.writestr(zinfo, data)
 
 

rules_python_zip/main_template.py
@@ -84,6 +84,10 @@ def main():
     exec ast in clean_globals
 
 
+# Environment variables that can change imports and break a pyz_binary
+_STRIP_ENVVARS=set(('PYTHONHOME', 'PYTHONPATH', 'PYTHONSTARTUP'))
+
+
 def reexec_without_site_packages_if_needed():
     # Attempt to isolate the Python environment as much as possible:
     # -S: Disable site module: disables .pth files that could be customized
@@ -92,7 +96,7 @@ def reexec_without_site_packages_if_needed():
     # TODO: Copy pex's "site cleaning" code to avoid re-executing?
     if 'site' in sys.modules:
         # re-exec without any PYTHON environment variables and without site packages
-        clean_env = {k: v for k, v in os.environ.items() if not k.startswith('PYTHON')}
+        clean_env = {k: v for k, v in os.environ.items() if k not in _STRIP_ENVVARS}
         # ensure runpy is available: virtualenv python -S does not have it
         import runpy
         clean_env['PYTHONPATH'] = os.path.dirname(runpy.__file__)

tests/python_envvars.py
@@ -1,0 +1,30 @@
+import os
+import subprocess
+import sys
+
+
+def print_env():
+    for key, value in os.environ.items():
+        print('{}={}'.format(key, value))
+
+
+def exec_self(path_to_execute):
+    # Execute ourselves with an environment variable set: this should be passed through
+    # Previously we stripped all PYTHON* environment variables which broke this
+    env = dict(os.environ)
+    env['PYTHON_EXAMPLE'] = 'should be found'
+    args = [path_to_execute, 'print_env']
+    print('executing', args)
+    subprocess.check_call(args, env=env)
+    print 'DONE'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: python_envvars.py (path to execute)\n')
+        sys.exit(1)
+
+    if sys.argv[1] == 'print_env':
+        print_env()
+    else:
+        exec_self(sys.argv[1])

commit 5b7c8559115070655e0473da61cfb3cca14b086a

tests/linkzip_test.py: Test linkzip.py permissions (#12)
Commit 3c704e909aa6 accidentally commited a fix to linkzip.py. This is the
test for that fix.

Also enable better test output from Bazel.

circleci.sh
@@ -9,7 +9,7 @@ chmod a+x ${BAZEL_INSTALLER}
 ./${BAZEL_INSTALLER} --user
 
 # Ensure everything can be built and tested
-~/bin/bazel test //...
+~/bin/bazel test --test_output=errors //...
 ~/bin/bazel build //...
 
 # Ensure the Go tools can be built

tools/bazel.rc
@@ -6,3 +6,6 @@
 # TODO: rules_pyz should be robust to this. In particular, it possibly should
 # not import site, which would avoid executing .pth files
 build --experimental_strict_action_env=true
+
+# More useful error output
+test --test_output=errors

tests/BUILD
@@ -326,3 +326,9 @@ pyz_output_test(
     expected_output="PYTHON_EXAMPLE=should be found",
     extra_arg="$(location :python_envvars_exezip)",
 )
+
+pyz_test(
+    name="linkzip_test",
+    srcs=["linkzip_test.py"],
+    data=["//rules_python_zip:linkzip.py"],
+)

tests/linkzip_test.py
@@ -1,0 +1,51 @@
+import json
+import os
+import shutil
+import subprocess
+import tempfile
+import unittest
+
+
+# relative to runfiles
+LINKZIP_PATH = 'rules_python_zip/linkzip.py'
+
+class TestLinkZip(unittest.TestCase):
+    def test_permissions(self):
+        tempdir = tempfile.mkdtemp()
+        try:
+            file_path = tempdir + '/file'
+            exe_path = tempdir + '/exe'
+            open(file_path, 'w').write('hello')
+            open(exe_path, 'w').write('#!/bin/sh\necho exe output\n')
+            os.chmod(exe_path, 0755)
+
+            output_path = tempdir + '/out.zip'
+            manifest = dict(
+                output_path=output_path,
+                interpreter_path='python',
+                files = [
+                    dict(src=file_path, dst='file'),
+                    dict(src=exe_path, dst='exe'),
+                ]
+            )
+            manifest_file = tempfile.NamedTemporaryFile()
+            json.dump(manifest, manifest_file)
+            manifest_file.flush()
+
+            subprocess.check_call((LINKZIP_PATH, manifest_file.name))
+
+            # use the external unzip command to unzip it
+            unzip_dir = tempdir + '/unzip_dir'
+            os.mkdir(unzip_dir)
+            # On Linux this exits with non-zero due to a warning about the header
+            subprocess.call(('unzip', '-d', unzip_dir, output_path))
+
+            # execute the exe
+            out = subprocess.check_output([unzip_dir + '/exe'])
+            self.assertEqual(out, 'exe output\n')
+
+            # read the file
+            data = open(unzip_dir + '/file').read()
+            self.assertEqual('hello', data)
+        finally:
+            shutil.rmtree(tempdir)