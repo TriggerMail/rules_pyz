TriggerMail/rules_pyz

commit ee7ecd7329ae462de773c870c260a4cc066c3e92

circleci.sh: Support and update to Bazel 0.16.0 (#30)
rules_python_zip.bzl: Remove deprecated attribute. Fixes:

rules_python_zip.bzl:28:13: Using cfg = "data" on an attribute is a noop
and no longer supported. Please remove it. You can use
--incompatible_disallow_data_transition=false to temporarily disable this
check.

circleci.sh
@@ -2,7 +2,7 @@
 set -euf -o pipefail
 
 # Download and install Bazel
-BAZEL_VERSION=0.14.0
+BAZEL_VERSION=0.16.0
 BAZEL_INSTALLER="bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
 wget https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${BAZEL_INSTALLER}
 chmod a+x ${BAZEL_INSTALLER}

rules_python_zip/rules_python_zip.bzl
@@ -27,7 +27,6 @@ _pyz_attrs = {
 
     "data": attr.label_list(
         allow_files = True,
-        cfg = "data",
     ),
 
     # this target's direct files must be unzipped to be executed. This is usually

commit 42b5fcb4665baa065912aa1c573903e7f222898d

testworkspace: Add a test workspace to CircleCI run with an older Bazel (#32)
testworkspace: Add a test workspace to CircleCI run with an older Bazel

We need to support some range of Bazel versions. Make sure the rules work
when imported as a third-party set of rules with an older version of Bazel.

rules_python_zip: Undo deprecated change that doesn't work with 0.14.1
tools/bazel.rc: Disable the deprecated warning for Bazel 0.16

Fixes error introduced by commit ee7ecd7329 for Bazel 0.14.1

circleci.sh
@@ -26,3 +26,24 @@ if [[ -n "$NOT_FORMATTED" ]]; then
     echo "$NOT_FORMATTED" > /dev/stderr
     exit 1
 fi
+
+# Ensure the rules work with older versions of Bazel
+# Copy the test workspace elsewhere and go to it
+# Installing Bazel inside a workspace executes tools/bazel.rc which can break old versions
+cp -r testworkspace /tmp
+COMMIT=$(git rev-parse HEAD)
+REMOTE=file://$(pwd)
+perl -pi -e "s/REPLACECOMMIT/${COMMIT}/" /tmp/testworkspace/WORKSPACE
+perl -pi -e "s#REPLACEREMOTE#${REMOTE}#" /tmp/testworkspace/WORKSPACE
+cd /tmp/testworkspace
+
+OLD_BAZEL_VERSION=0.14.1
+OLD_BAZEL_INSTALLER="bazel-${OLD_BAZEL_VERSION}-installer-linux-x86_64.sh"
+OLD_BAZEL_PREFIX=${HOME}/bazel-${OLD_BAZEL_VERSION}
+wget https://github.com/bazelbuild/bazel/releases/download/${OLD_BAZEL_VERSION}/${OLD_BAZEL_INSTALLER}
+chmod a+x ${OLD_BAZEL_INSTALLER}
+./${OLD_BAZEL_INSTALLER} --prefix=${OLD_BAZEL_PREFIX}
+
+${OLD_BAZEL_PREFIX}/bin/bazel test //...
+${OLD_BAZEL_PREFIX}/bin/bazel run //:binary
+${OLD_BAZEL_PREFIX}/bin/bazel version

rules_python_zip/rules_python_zip.bzl
@@ -27,6 +27,7 @@ _pyz_attrs = {
 
     "data": attr.label_list(
         allow_files = True,
+        cfg = "data",
     ),
 
     # this target's direct files must be unzipped to be executed. This is usually

testworkspace/BUILD
@@ -1,0 +1,11 @@
+load("@com_bluecore_rules_pyz//rules_python_zip:rules_python_zip.bzl", "pyz_binary", "pyz_test")
+
+pyz_test(
+    name="test",
+    srcs=["test.py"],
+)
+
+pyz_binary(
+    name="binary",
+    srcs=["binary.py"],
+)

testworkspace/WORKSPACE
@@ -1,0 +1,7 @@
+git_repository(
+    name = "com_bluecore_rules_pyz",
+    commit = "REPLACECOMMIT",
+    remote = "REPLACEREMOTE",
+)
+load("@com_bluecore_rules_pyz//rules_python_zip:rules_python_zip.bzl", "pyz_repositories")
+pyz_repositories()

testworkspace/binary.py
@@ -1,0 +1,8 @@
+#!/usr/bin/env python
+
+def main():
+    print('hello')
+
+
+if __name__ == '__main__':
+    main()

testworkspace/test.py
@@ -1,0 +1,5 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_nothing(self):
+        pass

tools/bazel.rc
@@ -14,7 +14,8 @@ test --test_output=errors
 # https://docs.bazel.build/versions/master/skylark/backward-compatibility.html
 #
 # rules_docker currently uses a bunch of incompatible flags; until it is fixed add incompatible changes then disable a few
-common --all_incompatible_changes --incompatible_no_support_tools_in_action_inputs=false --incompatible_new_actions_api=false
+# Bazel 0.14.1 requires cfg = "data" while Bazel 0.16 complains about it: disable for now
+common --all_incompatible_changes --incompatible_no_support_tools_in_action_inputs=false --incompatible_new_actions_api=false --incompatible_disallow_data_transition=false
 
 # Allow passing through docker environment variables: makes remote docker work on CircleCI
 build --action_env=DOCKER_TLS_VERIFY --action_env=DOCKER_HOST --action_env=DOCKER_CERT_PATH --action_env=DOCKER_MACHINE_NAME